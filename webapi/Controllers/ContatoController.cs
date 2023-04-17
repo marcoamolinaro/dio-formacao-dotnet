76e4fe6e56945cf806d1f5faf890c69f45352683
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2023-04-17 17:11:08 -0300

    Criando controller e endpoint create

---
+++ webapi/Controllers/ContatoController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using webapi.Context;
+using webapi.Entities;
+
+namespace webapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContatoController : ControllerBase
+    {
+
+        private readonly AgendaContext _context;
+
+        public ContatoController(AgendaContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult Create(Contato contato)
+        {
+            _context.Add(contato);
+            _context.SaveChanges();
+
+            return Ok(contato);
+        }
+        
+    }
+}
