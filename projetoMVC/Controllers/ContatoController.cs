9aec782b67f014b97ec9e50e2bb3fdcfba8481f0
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2023-05-10 14:24:30 -0300

    Detalhes do contato

--- projetoMVC/Controllers/ContatoController.cs
+++ projetoMVC/Controllers/ContatoController.cs
@@ -65,5 +65,17 @@ namespace projetoMVC.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Detalhes(int id)
+        {
+            var contato = _context.Contatos.Find(id);  
+            
+            if (contato == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(contato);
+        }
     }
 }
